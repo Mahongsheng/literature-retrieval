2790cac24ec4d435c3ecce9274ba9bb7a0ea5e68
mahongsheng <mahongsheng_cs@163.com>
2021-10-14 19:30:49 +0800

    初始化项目，添加文献实体类

---
+++ src/main/java/com/literature/retrieval/LiteratureRetrievalApplication.java
@@ -0,0 +1,13 @@
+package com.literature.retrieval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteratureRetrievalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteratureRetrievalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/literature/retrieval/LiteratureRetrievalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.literature.retrieval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteratureRetrievalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
